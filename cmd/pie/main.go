ce9fa7c75d13d4e93f8a1085556781710105685d
Ben Johnson <benbjohnson@yahoo.com>
2014-11-21 06:24:01 -0700

    Basic command line tool setup.

--- cmd/pie/main.go
+++ cmd/pie/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"flag"
+	"log"
+	"net/http"
+
 	"github.com/turingschool-examples/pie"
 )
 
 func main() {
+	// Parse command line flags.
+	addr := flag.String("addr", ":19876", "bind address")
+	flag.Parse()
+	log.SetFlags(0)
+
+	// Open database.
 	db := pie.NewDatabase()
+
+	// Initialize handler.
+	h := pie.NewHandler(db)
+
+	// Start HTTP handler.
+	log.Printf("Listening on http://localhost%s", *addr)
+	log.SetFlags(log.LstdFlags)
+	http.ListenAndServe(*addr, h)
 }
