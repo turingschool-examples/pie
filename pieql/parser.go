8333afadd8a0541cee6f69f613a1a219d637bbb7
Daniel Glunz <dannyglunz@gmail.com>
2014-12-04 19:49:44 -0700

    add parse for SELECT, field, FROM, and source

--- pieql/parser.go
+++ pieql/parser.go
@@ -1,12 +1,18 @@
 package pieql
 
 import (
+	"fmt"
 	"io"
 )
 
 // Parser represents a PieQL parser.
 type Parser struct {
-	s *Scanner
+	s   *Scanner
+	buf struct {
+		tok Token  // last read token
+		lit string // last read literal
+		n   int    // buffer size
+	}
 }
 
 // NewParser returns a new instance of Parser.
@@ -16,5 +22,69 @@ func NewParser(r io.Reader) *Parser {
 
 // Parse parses the next statement from the underlying reader.
 func (p *Parser) Parse() (*SelectStatement, error) {
-	panic("aaaaaaah!!!!")
+	stmt := &SelectStatement{}
+
+	if tok, lit := p.scanIgnoreWhitespace(); tok != SELECT {
+		return nil, fmt.Errorf("found %q, expected SELECT", lit)
+	}
+
+	for {
+		// Read a field.
+		tok, lit := p.scanIgnoreWhitespace()
+		if tok != IDENT && tok != MUL {
+			return nil, fmt.Errorf("found %q, expected field", lit)
+		}
+		stmt.Fields = append(stmt.Fields, &Field{lit})
+
+		// If the next token is not a comma then break the loop.
+		if tok, _ := p.scanIgnoreWhitespace(); tok != COMMA {
+			p.unscan()
+			break
+		}
+	}
+
+	// Next we expect to see the "FROM" keyword.
+	if tok, lit := p.scanIgnoreWhitespace(); tok != FROM {
+		return nil, fmt.Errorf("found %q, expected FROM", lit)
+	}
+
+	// Finally read in the source name.
+	tok, lit := p.scanIgnoreWhitespace()
+	if tok != IDENT {
+		return nil, fmt.Errorf("found %q, expected table name", lit)
+	}
+	stmt.Source = lit
+
+	return stmt, nil
+}
+
+// scan returns the next token from the scanner.
+// If a token been unscanned, read that instead.
+func (p *Parser) scan() (tok Token, lit string) {
+	// If we have a token on the buffer, return it.
+	if p.buf.n != 0 {
+		p.buf.n = 0
+		return p.buf.tok, p.buf.lit
+	}
+
+	// Otherwise read the next token from the scanner.
+	tok, lit = p.s.Scan()
+
+	// Save it to the buffer in case we need to unscan later.
+	p.buf.tok, p.buf.lit = tok, lit
+
+	return
+
+}
+
+// unscan pushes the previously read token back onto the buffer
+func (p *Parser) unscan() { p.buf.n = 1 }
+
+// scanIgnoreWhitespace scans the next non-whitespace token.
+func (p *Parser) scanIgnoreWhitespace() (tok Token, lit string) {
+	tok, lit = p.scan()
+	if tok == WS {
+		tok, lit = p.scan()
+	}
+	return
 }
